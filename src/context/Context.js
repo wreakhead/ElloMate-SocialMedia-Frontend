4eb11ffcb6bde517be2b178e463913953f08c576
wreakhead <mohdimran.khan751@gmail.com>
2021-09-15 00:28:56 +0530

    fixes

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,21 +3,7 @@ import { Reducer } from "./Reducer";
 
 const INITIAL_STATE = {
   user: {
-    profilePicture:
-      "https://media.comicbook.com/2021/07/rick-and-morty-season-5-episode-3-morty-adult-swim-1274605-1280x0.jpeg",
-    coverPicture: "https://wallpaperaccess.com/full/85323.png",
-    isAdmin: false,
-    _id: "611a2b87cfe2ce1e90465f7d",
-    username: "Morty",
-    email: "morty@gmail.com",
-    password: "$2b$10$D1BtVBeUzHMbQ2EnNmdu7e/aeFttGj6TXdyGmflfLlIfJ.q2i3wAm",
-    createdAt: "2021-08-16T09:10:31.091Z",
-    updatedAt: "2021-08-16T15:55:38.302Z",
-    __v: 0,
-    city: "New York",
-    describe: "I love my Inter-Dimentional travels",
-    from: "Earth 264",
-    relationship: 1,
+   
   },
   isFetching: false,
   error: false,
